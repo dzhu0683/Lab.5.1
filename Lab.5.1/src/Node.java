9419b5fecb441d3bafc37f083e88d0421833a5d4
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2018-05-31 09:10:58 -0400

    d

--- Lab.5.1/src/Node.java
+++ Lab.5.1/src/Node.java
@@ -1,4 +1,22 @@
+import java.lang.reflect.Array;
 
-public class Node {
+
+public class Node 
+{
+	public String city;
+	Node leftChild;
+	Node rightChild;
+	
+	public Node(String city, Node firstChild, Node secondChild)
+	{
+		this.city = city;
+		this.leftChild = firstChild;
+		this.rightChild = secondChild;
+	}
+	
+	public Arraylist<node> getChildren()
+	{
+		
+	}
 
 }
